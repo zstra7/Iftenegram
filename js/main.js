449b322987e952a489f24ed33d90ea06b8533e11
Zac Strauss <zstra7@hotmail.com>
2017-02-05 22:25:11 +1000

    ui changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ Vue.component('card', {
 	
 	data: function () {
 		return {
-			newComment: ''	
+			newComment: '',
+			grow: false,
+			liked: false
 		}
 	},
 
@@ -16,6 +18,10 @@ Vue.component('card', {
 				</span>
 			</div>
 			<div class="image">
+				<svg class="heart" v-bind:class="{ active: grow }" viewBox="0 0 32 29.6">
+  					<path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+					c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
+				</svg> 
 				<img v-bind:src="content.image" v-on:dblclick="increment(index)">
 			</div>
 			<section class="info-wrapper">
@@ -47,7 +53,12 @@ Vue.component('card', {
     	},
 
     	increment: function (index) {
-    		this.$emit('like', index);
+    		if (!this.liked) {
+    			this.$emit('like', index);
+    			this.grow = true;
+    			this.liked = true;
+    		}
+    		
     	}
 
 
@@ -61,7 +72,7 @@ var app = new Vue({
     posts: [
     	{ name : 'Alex', image: 'img/1.jpg', location: "NYC", comments: ["yeah nice","wow"], likes: 43},
     	{ name : 'Jake', image: 'img/2.jpg', location: "Kingaroy", comments: ["yeah nice","wow"], likes: 66},
-    	{ name : 'Karl', image: 'img/1.jpg', location: "Brisbane", comments: [], likes: 2}
+    	{ name : 'Karl', image: 'img/3.jpg', location: "Brisbane", comments: [], likes: 2}
     ]
 	},
 	methods: {
