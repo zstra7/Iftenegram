d7ffeb94c6379e570ee78287ba4cdd5bc0f0550a
Zac Strauss <zstra7@hotmail.com>
2017-02-05 15:54:57 +1000

    Ui and like funct

--- js/main.js
+++ js/main.js
@@ -10,27 +10,32 @@ Vue.component('card', {
 	template: `
 		<div class="card">
 			<div class="header">
-				<span>
+				<span class="name">
+					<span class="header-image"></span>
 					{{ content.name }}
-				</span>
-				<span>
-					{{ content.location }}
 				</span>
 			</div>
 			<div class="image">
-				<img v-bind:src="content.image" width="100px">
+				<img v-bind:src="content.image" v-on:dblclick="increment(index)">
 			</div>
-			<div class="comments">
-				<span v-for="comment in content.comments">
-					{{ comment }}
-				</span>
-			</div>
-			
-			<input
-    			v-model="newComment"
-    			v-on:keyup.enter="sendComment(index)"
-    			placeholder="Add a comment"
-  			>
+			<section class="info-wrapper">
+				<div class="likes">
+					{{ content.likes }} likes
+				</div>
+ 				<div class="comments">
+					<span class="comment" v-for="comment in content.comments">
+						<span class="author">Alex Iftene</span>
+						{{ comment }}
+					</span>
+				</div>
+				<div class="divider"></div>
+				<input
+    				v-model="newComment"
+    				v-on:keyup.enter="sendComment(index)"
+    				placeholder="Add a comment"
+    				class="message-input"
+  				>
+  			</section>
 		</div>
 	`,
 	methods: {
@@ -39,25 +44,32 @@ Vue.component('card', {
       		this.newComment = '';
       		console.log(index);
       		
+    	},
+
+    	increment: function (index) {
+    		this.$emit('like', index);
     	}
+
+
   	},
   	props: ['content', 'index']
 })
-
-
 
 var app = new Vue({
  	el: '#app',
  	data: {
     posts: [
-    	{ name : 'Alex', image: 'img/1.jpg', location: "NYC", comments: ["yeah nice","wow"], likes: 0},
-    	{ name : 'jake', image: 'img/1.jpg', location: "Kingaroy", comments: ["yeah nice","wow"], likes: 0},
-    	{ name : 'Karl', image: 'img/1.jpg', location: "Brisbane", comments: [], likes: 0}
+    	{ name : 'Alex', image: 'img/1.jpg', location: "NYC", comments: ["yeah nice","wow"], likes: 43},
+    	{ name : 'Jake', image: 'img/2.jpg', location: "Kingaroy", comments: ["yeah nice","wow"], likes: 66},
+    	{ name : 'Karl', image: 'img/1.jpg', location: "Brisbane", comments: [], likes: 2}
     ]
 	},
 	methods: {
   		addComment: function (comment,index) {
   			this.$data.posts[index].comments.push(comment);
+  		},
+  		addLike: function (index) {
+  			this.$data.posts[index].likes += 1;
   		}
   	} 
 })
